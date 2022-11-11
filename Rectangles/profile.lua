02733922cfde65543e74e0c4b4875c3dcd132b55
Elwex123 <elyasaf123456789@gmail.com>
2022-11-11 15:44:17 +0200

    Refine profiling

--- Rectangles/profile.lua
+++ Rectangles/profile.lua
@@ -2,7 +2,7 @@ local rect = require("counter")
 local shapes = require("shapes")
 
 for _ = 1, math.pow(10, 5) do
-  for _, shape in ipairs(shapes) do
-    rect.count(shape.q)
-  end
+	for _, shape in ipairs(shapes) do
+		rect.count(shape.q)
+	end
 end
