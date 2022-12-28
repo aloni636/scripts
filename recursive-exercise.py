cfe4035f51ac6eeda5ae1b7af6500d28a14e2d7a
Elwex123 <elyasaf123456789@gmail.com>
2022-12-28 22:17:45 +0200

    Temp

--- recursive-exercise.py
+++ recursive-exercise.py
@@ -1,44 +1,23 @@
-class Context:
-    def _init_(self, max_level=5) -> None:
-        self.max_level = max_level
-        self.current_level = max_level
-        self.current_char = 0
-        self.stdout: str = ''
+from math import floor, ceil
 
-    def recurse(self):
-        # Break condition
-        if -(self.current_level) > self.max_level:
+
+def recurse(level: float) -> str:
+    # level: int needs to encode 2 things:
+    # A. Max Height
+    # B.
+    # base case
+    if True:
+        return ""
+    # recursive case
+    else:
+        # positive level: decreasing lines length
+        if level > 0:
+            recurse(level-(1/ceil(level)))
             pass
-        # Middle control
-        elif self.current_level == 0:
-            self.current_level = -2
-            self.current_char = 0
-            self.recurse()
-        # Characters control
+            # level-ceil()
+            # return "!" + recurse(level-)
+        elif level == 0:
+            pass
+        # negative level: increasing lines length
         else:
-            if self.current_char+1 == self.max_level+abs(self.current_level):
-                self.current_level -= 1
-                self.current_char = 0
-                self.stdout += '\n'
-                self.recurse()
-            else:
-                char = ''
-                max_current_diff = self.max_level-abs(self.current_level)
-
-                if self.current_char < max_current_diff:
-                    char = ' '
-                # compensate for space characters on odd levels
-                elif (self.current_char+max_current_diff) % 2 == 0:
-                    char = '!'
-                else:
-                    char = '.'
-                self.current_char += 1
-                self.stdout += char
-                self.recurse()
-
-
-for i in range(1, 5+1, 1):
-    print(f'paremeter: {i}\n============')
-    c = Context(i)
-    c.recurse()
-    print(c.stdout)
+            # return "." + recurse(level-1)
